424c36353697b3f003cf20959fec03990b44141c
Ellie Farrer <elliefarrermusic@gmail.com>
2018-10-31 12:24:42 +0000

    Built basic Dog class to try out inheritence

---
+++ animals/Dog.java
@@ -0,0 +1,20 @@
+class Dog {
+
+  public Dog() {
+
+  }
+
+  public void bite() {
+    System.out.println("Your dog took a bite!");
+  }
+
+  public void jump() {
+    System.out.println("Your dog jumped!");
+  }
+
+  public static void main(String[] args) {
+    Dog leila = new Dog();
+    leila.bite();
+    leila.jump();
+  }
+}
